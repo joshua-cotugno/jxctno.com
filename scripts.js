19e89cd6859d6e0e95c1a8867a12cedf0ca0aa01
jxctno <jdcotugno006@gmail.com>
2022-12-02 14:18:25 +0000

    Launch

---
+++ scripts.js
@@ -0,0 +1,57 @@
+const today_date = new Date();
+
+let day = Number(today_date.getDate());
+
+if (today_date.getDate() == 1 || today_date.getDate() == 21) {
+    superscript = "st"
+} else if (today_date.getDate() == 2 || today_date.getDate() == 22) {
+    superscript = "nd"
+} else if (today_date.getDate() == 3 || today_date.getDate() == 23) {
+    superscript = "rd"
+} else {
+    superscript = "th"
+} // get the bit after 1st/2nd for date
+
+function today() {
+    document.getElementById("todaydate").innerHTML = `Today is the ${day}<sup>${superscript}</sup> of December`
+}
+
+
+function openDay(doorNumber) {
+    if (doorNumber <= day) {
+        opendoor(doorNumber)
+    } else {
+        alert(`Not so fast!!! \nYou can't open a door until it's day! \nYou can only open up to day ${day} today!`)
+    }
+}
+
+//          DAY | 1
+const messageHeaders = ["December 1<sup>st</sup>", "December 2<sup>nd</sup>",]
+const messageText = ["One of the first images I have...<br>There will be more- though some you will find embarrasing lol", 
+                    "I dont have a correct date for this one...<br>I think its us on the way home from Cambridge.<br>I just think its a really cool photo lol 👌",
+                ]
+
+function opendoor(doorNumber) {
+    document.getElementById(`door${doorNumber}`).classList.replace("closed","open")
+    document.getElementById(`door${doorNumber}`).style.backgroundImage = "url('./media/opendoor.png')"
+    play()
+    setTimeout(10)
+    document.getElementById("message").style.visibility = "visible"; // Open message
+    document.getElementById("calendar").style.visibility = "hidden";
+    let image = `./media/img (${doorNumber}).jpg`
+    document.getElementById("message_image").setAttribute("src", image);
+    document.getElementById("messageHeader").innerHTML = messageHeaders[doorNumber - 1]
+
+    document.getElementById("messageText").innerHTML = messageText[doorNumber - 1]
+}
+
+function closemessage() {
+    document.getElementById("message").style.visibility = "hidden"; // close message
+    document.getElementById("calendar").style.visibility = "visible";
+}
+
+
+function play() {
+    var audio = document.getElementById("audio");
+    audio.play();
+}
