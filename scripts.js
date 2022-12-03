1bc69b3f00702d189442f54ea50ce8944d83efa6
blue-horizons <66258429+blue-horizons@users.noreply.github.com>
2022-12-03 18:42:40 +0000

    day 3

--- scripts.js
+++ scripts.js
@@ -1,5 +1,14 @@
 const today_date = new Date();
 
+//          DAY | 1
+const messageHeaders = ["December 1<sup>st</sup>", "December 2<sup>nd</sup>","December 3<sup>rd</sup>"]
+const messageText = ["One of the first images I have...<br>There will be more- though some you will find embarrasing lol", 
+                    "I dont have a correct date for this one...<br>I think its us on the way home from Cambridge.<br>I just think its a really cool photo lol 👌",
+                "Brighton,2nd August- a nice beach view to soothe your soul today."]
+
+
+
+
 let day = Number(today_date.getDate());
 
 if (today_date.getDate() == 1 || today_date.getDate() == 21) {
@@ -24,12 +33,6 @@ function openDay(doorNumber) {
         alert(`Not so fast!!! \nYou can't open a door until it's day! \nYou can only open up to day ${day} today!`)
     }
 }
-
-//          DAY | 1
-const messageHeaders = ["December 1<sup>st</sup>", "December 2<sup>nd</sup>",]
-const messageText = ["One of the first images I have...<br>There will be more- though some you will find embarrasing lol", 
-                    "I dont have a correct date for this one...<br>I think its us on the way home from Cambridge.<br>I just think its a really cool photo lol 👌",
-                ]
 
 function opendoor(doorNumber) {
     document.getElementById(`door${doorNumber}`).classList.replace("closed","open")
