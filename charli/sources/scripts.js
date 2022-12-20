37d143b80cdd724995b5f7e39e8b177ee9f678eb
jxctno <jdcotugno006@gmail.com>
2022-12-20 18:03:47 +0000

    more edits

--- charli/sources/scripts.js
+++ charli/sources/scripts.js
@@ -6,6 +6,9 @@ function loadScript() {
 
 function clickme(){
     alert("You have no idea wht i could do to this phone lol")
+    
+    var music = new Audio("sources/wintering.mp3");
+    music.play();
     for (i=0;i<ids.length;i++){
         document.getElementById(ids[i]).style.visibility = "visible"
     }
