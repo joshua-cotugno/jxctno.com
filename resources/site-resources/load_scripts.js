586eb42390529bef1ebe70391750dcf7ba1d9d95
blue-horizons <jojotraingeek@gmail.com>
2023-02-08 17:15:20 +0000

    Added a 404 page, added contact info, added some audio files, and images for the home page

---
+++ resources/site-resources/load_scripts.js
@@ -0,0 +1,18 @@
+// function decideStyles() {
+//     if (window.innerWidth <= 767) {
+//         console.log("Displaying in mobile mode")
+//         var link = document.createElement("link");
+//         link.rel = "stylesheet";
+//         link.href = "resources/site-resources/mobile-styles.css";
+//         document.head.appendChild(link);
+//     } else {
+//         var link = document.createElement("link");
+//         link.rel = "stylesheet";
+//         link.href = "resources/site-resources/global-styles.css";
+//         document.head.appendChild(link);
+//     }
+    
+//     document.body.removeAttribute("hidden")
+// }
+
+// window.onload = decideStyles;
